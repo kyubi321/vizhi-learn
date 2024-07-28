a22a8e32b50f406d465f43ab9c5b8000d77a301b
amar <amarnathkco7@gmail.com>
2024-07-28 09:45:46 +0530

    added the image picker

--- src/app/(tabs)/index copy.tsx
+++ src/app/(tabs)/index copy.tsx
@@ -1,18 +1,42 @@
 import { Image, Text, View,TextInput, Pressable} from "react-native"
 import { FontAwesome6 } from '@expo/vector-icons';
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import * as ImagePicker from 'expo-image-picker';
 
 export default function create(){
     // now to store the value for other purposes we use usestate
     const [caption, setCaption]=useState('');
+    const [image, setImage]=useState<string | null>(null);
+
+    useEffect(()=>{
+        if(!image){
+            pickImage();
+        }
+    },[image]);
+
+    const pickImage = async () => {
+        // No permissions request is necessary for launching the image library
+        let result = await ImagePicker.launchImageLibraryAsync({
+          mediaTypes: ImagePicker.MediaTypeOptions.Images,
+          allowsEditing: true,
+          aspect: [4, 3],
+          quality: 1,
+        });
+    
+        console.log(result);
+    
+        if (!result.canceled) {
+          setImage(result.assets[0].uri);
+        }
+    };
 
     return( 
         <View className="p-3 items-center">
             <Image source={{
-                uri:'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/images/2.jpg',
+                uri:image,
             }} 
-            className=" w-64 aspect-[3/4] rounded-lg"/>
-            <Text onPress={()=>{}} className="text-blue-500 text-2xl font-semibold p-3"> Change</Text>
+            className=" w-64 aspect-[3/4] rounded-lg bg-slate-400"/>
+            <Text onPress={pickImage} className="text-blue-500 text-2xl font-semibold p-3"> Change</Text>
             <TextInput
             value={caption}
             onChangeText={(newValue)=>setCaption(newValue)}
