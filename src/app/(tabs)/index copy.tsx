80c48706e8f339f3280f6407afdfdf9a1928db46
amar <amarnathkco7@gmail.com>
2024-08-01 10:44:34 +0530

    upload function

--- src/app/(tabs)/index copy.tsx
+++ src/app/(tabs)/index copy.tsx
@@ -3,6 +3,21 @@ import { FontAwesome6 } from '@expo/vector-icons';
 import { useEffect, useState } from "react";
 import * as ImagePicker from 'expo-image-picker';
 import Button from "../../components/Button";
+import { Cloudinary } from "@cloudinary/url-gen";
+import { uploadImage } from "../../lib/cloudinary";
+
+// somehow the upload function is not working.
+export const cld = new Cloudinary({
+    cloud: {
+        cloudName:process.env.EXPO_PUBLIC_CLOUDINARY_CLOUD_NAME,
+    },
+    url:{
+        secure: true
+    }
+});
+
+
+
 export default function create(){
     // now to store the value for other purposes we use usestate
     const [caption, setCaption]=useState('');
@@ -20,7 +35,7 @@ export default function create(){
           mediaTypes: ImagePicker.MediaTypeOptions.Images,
           allowsEditing: true,
           aspect: [4, 3],
-          quality: 1,
+          quality: 0.5,
         });
     
         console.log(result);
@@ -28,6 +43,15 @@ export default function create(){
         if (!result.canceled) {
           setImage(result.assets[0].uri);
         }
+    };
+
+    const createPost = async () => {
+        if (!image){
+            return;
+        }
+        const response = await uploadImage(image);
+        console.log('image id: ',response?.public_id);
+        
     };
 
     return( 
@@ -42,7 +66,7 @@ export default function create(){
             onChangeText={(newValue)=>setCaption(newValue)}
              placeholder="Whats in your mind?" className="w-full"></TextInput>
              <View>
-            <Button title="Share" className="w-full bg-blue-500 p-4 items-center mt-80 rounded-xl"/></View>
+            <Button title="Share" className="w-full bg-blue-500 p-4 items-center mt-80 rounded-xl" onPress={createPost}/></View>
         </View>
 )
 
--- src/lib/cloudinary.ts
+++ src/lib/cloudinary.ts
@@ -1,8 +1,39 @@
 import { Cloudinary } from "@cloudinary/url-gen";
+import { upload } from 'cloudinary-react-native';
+import { UploadApiOptions,UploadApiResponse } from "cloudinary-react-native/lib/typescript/src/api/upload/model/params/upload-params";
 
 export const cld = new Cloudinary({
     cloud: {
         cloudName:process.env.EXPO_PUBLIC_CLOUDINARY_CLOUD_NAME,
     },
+    url:{
+        secure: true
+    }
 });
 //this is the cloudinary instance 
+
+export const uploadImage = async (file: string) => {
+    const options: UploadApiOptions = {
+        upload_preset: 'default',
+        unsigned: true,
+    };
+    return new Promise<UploadApiResponse>(async(resolve,reject)=>{
+        await upload(cld, {
+            file,
+            options: options,
+            callback: (error, response) => {
+                if (error || !response){
+                    reject(error);
+                } else {
+                    resolve(response);
+                }   
+            },
+        });
+
+
+    });
+
+    
+};
+
+
